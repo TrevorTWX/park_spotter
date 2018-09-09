20ed738612023229c9537923fbaafca180d02e01
Hank860502 <hank860502@gmail.com>
2018-09-09 01:03:09 -0400

    when click showMap bg video change

--- public/js/coming-soon.js
+++ public/js/coming-soon.js
@@ -1,11 +1,5 @@
 (function($) {
   "use strict"; // Start of use strict
-	$('#myModal').modal(options)
-	$('#myModal').modal('show')
-	$('#myModal').modal('hide')
-	$('#myModal').modal('handleUpdate')
-	$('#myModal').modal('toggle')
-
   // No JS
 
 })(jQuery); // End of use strict
