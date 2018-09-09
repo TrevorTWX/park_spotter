7c74c2814cc07696b4a558db70e05153b3bd3e97
Hank860502 <hank860502@gmail.com>
2018-09-08 22:59:41 -0400

    admin page add two form

--- public/js/coming-soon.js
+++ public/js/coming-soon.js
@@ -1,9 +1,11 @@
 (function($) {
   "use strict"; // Start of use strict
 	$('#myModal').modal(options)
-	$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
+	$('#myModal').modal('show')
+	$('#myModal').modal('hide')
+	$('#myModal').modal('handleUpdate')
+	$('#myModal').modal('toggle')
+
   // No JS
 
 })(jQuery); // End of use strict
