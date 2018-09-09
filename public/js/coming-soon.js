9b4520ea800f219f010185782053954c10189ab0
Hank860502 <hank860502@gmail.com>
2018-09-08 21:21:01 -0400

    show map button failed to pop modal

--- public/js/coming-soon.js
+++ public/js/coming-soon.js
@@ -1,6 +1,9 @@
 (function($) {
   "use strict"; // Start of use strict
-
+	$('#myModal').modal(options)
+	$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
   // No JS
 
 })(jQuery); // End of use strict
